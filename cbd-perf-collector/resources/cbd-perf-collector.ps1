a61280be598c8ce22a1f5e343354617ec42165f7
Robert Goltz <github@rgoltz.de>
2021-01-11 14:24:48 +0100

    Adding awscli put-metric-data
    adding awscli cmds for cloudwatch put-metric-data for WriteLatency, ReadLatency, DeleteLatency

--- cbd-perf-collector/resources/cbd-perf-collector.ps1
+++ cbd-perf-collector/resources/cbd-perf-collector.ps1
@@ -1,5 +1,5 @@
 ﻿<# 
-.SYNOPSIS CloudBerry Drive performance checks with AWS CloudWatch integration
+.SYNOPSIS CloudBerry Drive performance probs with AWS CloudWatch integration
 .DESCRIPTION CloudBerry Drive performance checks with AWS CloudWatch integration using Copy-Item / Compare-Object and Remove-Item
 .NOTES This PowerShell script was developed and optimized for ScriptRunner. 
 The script will generate a random filename/filecontent along a fixed filesize.
@@ -7,8 +7,16 @@ The time (in ms) to Copy and Remove a file will be collected and pushed to Cloud
 The content of the orginal file and the file after copy to CloudBerry will be compared and result will be pushed to a CloudWatch Metric as well.
 Version:
 - 1.0 Robert Goltz: initial version without awscli/put-metric-data after local testing finished successfully
+- 1.1 Robert Goltz: define Get-Content / read-operation as dedicated check
+- 2.0 Robert Goltz: adding awscli cmds for cloudwatch put-metric-data for WriteLatency, ReadLatency, DeleteLatency
 .LINK https://github.com/<to-be-uploaded>
 #>
+
+# TODO: check if local folder exists (LocalWindows*)
+# TODO: impl check for target-folder (to avoid typos or issues with Remove-part of the script)
+# TODO: impl logfile rolling/housekeeping
+# TODO: check, if there is already an instance of the script is running = lock-file
+# TODO: if diff is not OK, copy both files to a dlq folder to enable further checks
 
 
 $localHostname = [System.Net.Dns]::GetHostName()
@@ -18,20 +26,27 @@ $localHostname = [System.Net.Dns]::GetHostName()
 #################
 # !! do not add "\" on the end of the following variables:
 # local folder, e.g. on C-drive
-##$LocalWindowsDriveFolder = "D:\CloudWatch_cbd-perf-collector\temp"
-$LocalWindowsDriveFolder = "C:\temp\cbd-source"
+$LocalWindowsDriveFolder = "D:\CloudWatch_cbd-perf-collector\temp"
+##$LocalWindowsDriveFolder = "C:\temp\cbd-source"
 # folder on CloudBerry Drive, e.g. F-drive
-##$CloudBerryDriveFolder = "F:\PKSPG_SFS\att"
-$CloudBerryDriveFolder = "C:\temp\cbd-target"
+$CloudBerryDriveFolder = "F:\PKSPG_SFS\att"
+##$CloudBerryDriveFolder = "C:\temp\cbd-target"
 # local folder for logging if this script
-##$LocalWindowsLogFolder = "D:\CloudWatch_cbd-perf-collector\logs"
-$LocalWindowsLogFolder = "C:\temp\cbd-perf-collector-logs"
+$LocalWindowsLogFolder = "D:\CloudWatch_cbd-perf-collector\logs"
+##$LocalWindowsLogFolder = "C:\temp\cbd-perf-collector-logs"
 
-# TODO: check if local folder exists (LocalWindows*)
-# TODO: impl check for target-folder (to avoid typos or issues with Remove-part of the script)
-# TODO: impl logfile rolling/housekeeping
-# TODO: check, if there is already an instance of the script is running = lock-file
-# TOFO: if diff is not OK, copy both files to a dlq folder to enable further checks
+
+#####
+# cloudwatch settings
+#####
+# note: level within CloudWatch metric within AWS Console:
+    # (1st level) namespace:  => cloudwatchRegion
+    # (2nd level) dimensions: => cloudwatchDimension
+    # (3rd level) tableview:  => stepScope
+$cloudwatchRegion = "eu-central-1"
+$cloudwatchMetricname = "elokpkh-Win-CloudBerryDrive-CustomMetrics"
+$instanceId = (Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/instance-id).content
+
 
 ####################################
 
@@ -60,11 +75,8 @@ function Get-RandomHex {
 ####################################
 
 #################
-# step 0: cleanup temp-folder and generate file with random context (by having a fixed content-size)
+# step 0: generate file with random content (by having a fixed content-size)
 #################
-
-# delete all files within local working-dir
-#Remove-Item $LocalWindowsDriveFolder\*.*
 
 Write-Log -Message "---> START: starting PowerShell script on $localHostname & generating filename/filecontent ..."
 # generate and set random filename (e.g. A19E2C348E6BFF373172131F6A1241.txt = 123 Bits)
@@ -73,6 +85,7 @@ $randomFileNameHex = Get-RandomHex -Bits 123
 $randomFileContentLocalHex = Get-RandomHex -Bits 1234567
 $randomFileContentLocalHex | Out-File -FilePath "$LocalWindowsDriveFolder\$randomFileNameHex.txt"
 
+# set variables with full path and filename
 $SourcePathAndFile = "$LocalWindowsDriveFolder\$randomFileNameHex.txt"
 $DestinationPathAndFile = "$CloudBerryDriveFolder\$randomFileNameHex.txt"
 
@@ -81,6 +94,8 @@ $DestinationPathAndFile = "$CloudBerryDriveFolder\$randomFileNameHex.txt"
 #################
 # step 1: write file to CloudBerry Drive folder and track time for this step in ms
 #################
+$stepScope = "WriteLatency"
+$cloudwatchDimension = "CloudBerryMonitoringScope=PerformanceProbe"
 $stopwatch = New-Object System.Diagnostics.Stopwatch
 $stopwatch.Start()
 Try
@@ -89,8 +104,8 @@ Try
     $stopwatch.Stop()
     $CopyStepElapsedMilliseconds = $stopwatch.ElapsedMilliseconds
     Write-Log -Message " OK: Copy-Item from: $SourcePathAndFile OK after $CopyStepElapsedMilliseconds ms to: $DestinationPathAndFile."
-    # awscli for put-metric-data
-    Write-Log -Message "  --> CloudWatch-writeFileTime: $CopyStepElapsedMilliseconds for Copy-Item pushed to CloudWatch."
+    aws cloudwatch put-metric-data --metric-name "$stepScope" --namespace "$cloudwatchMetricname" --dimensions "InstanceId=$instanceId,$cloudwatchDimension" --unit Milliseconds --value $CopyStepElapsedMilliseconds --region "$cloudwatchRegion"  
+    Write-Log -Message "  --> CloudWatch -> $stepScope : $CopyStepElapsedMilliseconds for cmd Copy-Item pushed to CloudWatch (see metric: $cloudwatchMetricname -> $cloudwatchDimension)."
 }
 Catch
 {
@@ -105,12 +120,42 @@ Catch
 
 
 #################
-# step 2: compare the content of local file against the content of file, which has been written to CloudBerry Drive folder before
+# step 2: read file from CloudBerry Drive folder and track time for this step in ms
 #################
+$stepScope = "ReadLatency"
+$cloudwatchDimension = "CloudBerryMonitoringScope=PerformanceProbe"
+$stopwatch = New-Object System.Diagnostics.Stopwatch
+$stopwatch.Start()
+Try
+{
+    $ContentTargetFile = Get-Content $DestinationPathAndFile
+    $stopwatch.Stop()
+    $ReadStepElapsedMilliseconds = $stopwatch.ElapsedMilliseconds
+    Write-Log -Message " OK: Get-Content from: $DestinationPathAndFile OK after $CopyStepElapsedMilliseconds ms."
+    aws cloudwatch put-metric-data --metric-name "$stepScope" --namespace "$cloudwatchMetricname" --dimensions "InstanceId=$instanceId,$cloudwatchDimension" --unit Milliseconds --value $ReadStepElapsedMilliseconds --region "$cloudwatchRegion"  
+    Write-Log -Message "  --> CloudWatch -> $stepScope : $ReadStepElapsedMilliseconds for cmd Copy-Item pushed to CloudWatch (see metric: $cloudwatchMetricname -> $cloudwatchDimension)."
+}
+Catch
+{
+    $ErrorMessage = $_.Exception.Message
+    $FailedItem = $_.Exception.ItemName
+    $stopwatch.Stop()
+    $FailedElapsedMilliseconds = $stopwatch.ElapsedMilliseconds
+    Write-Log -Message " ERROR-catched: Get-Content failed with $ErrorMessage on $FailedItem after $FailedElapsedMilliseconds ms."
+}
+
+
+
+
+#################
+# step 3: compare the content of local file against the content of file, which has been written to CloudBerry Drive folder before
+#################
+$stepScope = "IsContentSame"
+$cloudwatchDimension = "CloudBerryMonitoringScope=IntegrityProbe"
 Try
 {
     $ContentSourceFile = $randomFileContentLocalHex
-    $ContentTargetFile = Get-Content $DestinationPathAndFile
+    #ContentTargetFile is already set above for reading the file from CloudBerry
     if (Compare-Object $ContentSourceFile $ContentTargetFile)  {
         Write-Log -Message " WARN: Compare-Object - content of both files are NOT the same!"
         # awscli put-metric-data for 1, since strings are different
@@ -125,15 +170,17 @@ Catch
 {
     $FailedItem = $_.Exception.ItemName
     $FailedElapsedMilliseconds = $stopwatch.ElapsedMilliseconds
-    Write-Log -Message " ERROR-catched: compareFileContent failed with $ErrorMessage on $FailedItem"
+    Write-Log -Message " ERROR-catched: Compare-Object failed with $ErrorMessage on $FailedItem"
 }
 
 
 
 
 #################
-# step 3: delete file from CloudBerry Drive folder and track time for this step in ms
+# step 4: delete file from CloudBerry Drive folder and track time for this step in ms
 #################
+$stepScope = "DeleteLatency"
+$cloudwatchDimension = "CloudBerryMonitoringScope=PerformanceProbe"
 $stopwatch = New-Object System.Diagnostics.Stopwatch
 $stopwatch.Start()
 Try
@@ -142,8 +189,8 @@ Try
     $stopwatch.Stop()
     $RemoveStepElapsedMilliseconds = $stopwatch.ElapsedMilliseconds
     Write-Log -Message " OK: Remove-Item $DestinationPathAndFile OK after $RemoveStepElapsedMilliseconds ms."
-    # awscli for put-metric-data
-    Write-Log -Message "  --> CloudWatch-deleteFileTime: $RemoveStepElapsedMilliseconds for Remove-Item pushed to CloudWatch."
+    aws cloudwatch put-metric-data --metric-name "$stepScope" --namespace "$cloudwatchMetricname" --dimensions "InstanceId=$instanceId,$cloudwatchDimension" --unit Milliseconds --value $RemoveStepElapsedMilliseconds --region "$cloudwatchRegion"  
+    Write-Log -Message "  --> CloudWatch -> $stepScope : $RemoveStepElapsedMilliseconds for cmd Copy-Item pushed to CloudWatch (see metric: $cloudwatchMetricname -> $cloudwatchDimension)."
 }
 Catch
 {
